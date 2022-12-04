97c326fb8c439b2e89aa0394ad5a303916e7b962
Donnie Laughton <donnie.laughton@recursionpharma.com>
2022-12-03 20:56:45 -0500

    fun alternate for day03

--- 2022/day03/day03_alternate.py
+++ 2022/day03/day03_alternate.py
@@ -9,21 +9,16 @@ def s1(inventory):
     compartment1 = set(inventory[:num_items//2])
     compartment2 = set(inventory[num_items//2:])
     res, = compartment1 & compartment2
-    assert len(res) == 1
     return get_priority(res)
 
-def s2(inventories):
-    badges = []
-    for i in range(0, len(inventories), 3):
-        elf1 = set(inventories[i].strip())
-        elf2 = set(inventories[i+1].strip())
-        elf3 = set(inventories[i+2].strip())
-        res = elf1 & elf2 & elf3
-        assert len(res) == 1
-        badges.append(get_priority(res.pop()))
-    return sum(badges)
+def s2(group):
+    elf1 = set(group[0].strip())
+    elf2 = set(group[1].strip())
+    elf3 = set(group[2].strip())
+    res, = elf1 & elf2 & elf3
+    return get_priority(res)
 
-with open("day03_data.txt") as f:
+with open("./2022/day03/day03_data.txt") as f:
     lines = f.readlines()
     print(f" s1: {sum(map(s1, lines))}")
-    print(f" s2: {s2(lines)}")
+    print(f" s2: {sum(map(s2, [lines[i:i+3] for i in range(0, len(lines), 3)]))}")
