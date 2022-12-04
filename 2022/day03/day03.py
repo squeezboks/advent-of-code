d8611d537b6a9d20017065863cb92a51eb4762a7
Donnie Laughton <dlaughton@gmail.com>
2022-12-03 20:28:13 -0500

    Advent of Code 2022 - Day 3 Solution (#3)

---
+++ 2022/day03/day03.py
@@ -0,0 +1,32 @@
+def get_priority(item):
+    item = ord(item)
+    priority = item - 96 if item > 90 else item - 38
+    return priority
+
+def s1(inventories):
+    rucksacks = []
+    for inventory in inventories:
+        inventory = inventory.strip()
+        num_items = len(inventory)
+        compartment1 = set(inventory[:num_items//2])
+        compartment2 = set(inventory[num_items//2:])
+        res = compartment1 & compartment2
+        assert len(res) == 1
+        rucksacks.append(get_priority(res.pop()))
+    return sum(rucksacks)
+
+def s2(inventories):
+    badges = []
+    for i in range(0, len(inventories), 3):
+        elf1 = set(inventories[i].strip())
+        elf2 = set(inventories[i+1].strip())
+        elf3 = set(inventories[i+2].strip())
+        res = elf1 & elf2 & elf3
+        assert len(res) == 1
+        badges.append(get_priority(res.pop()))
+    return sum(badges)
+
+with open("day03_data.txt") as f:
+    lines = f.readlines()
+    print(f" s1: {s1(lines)}")
+    print(f" s2: {s2(lines)}")
