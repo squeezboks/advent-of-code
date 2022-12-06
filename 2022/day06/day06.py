1a97f65b6270aa147e235a508fc34609bc829b2d
Donnie Laughton <donnie.laughton@recursionpharma.com>
2022-12-06 09:43:02 -0500

    cleaner_solution

--- 2022/day06/day06.py
+++ 2022/day06/day06.py
@@ -1,26 +1,20 @@
+from collections import deque
+
 def s1(datastream):
-    exit = False
-    pos = 0
-    while not exit:
-        unique_char = len(set(datastream[pos:pos+4]))
-        if unique_char != 4:
-            pos += 1
-            exit = False
-        else:
-            exit = True
-    return (pos+4)
+    window = deque(maxlen=4)
+    for index, char in enumerate(datastream):
+        window.append(char)
+        if len(set(window)) == 4:
+            return index + 1
+    raise Exception('no start of packet header')
 
 def s2(datastream):
-    exit = False
-    pos = 0
-    while not exit:
-        unique_char = len(set(datastream[pos:pos+14]))
-        if unique_char != 14:
-            pos += 1
-            exit = False
-        else:
-            exit = True
-    return (pos+14)
+    window = deque(maxlen=14)
+    for index, char in enumerate(datastream):
+        window.append(char)
+        if len(set(window)) == 14:
+            return index + 1
+    raise Exception('no start of message header')
 
 with open("./2022/day06/day06_data.txt") as f:
     datastream = f.read()
