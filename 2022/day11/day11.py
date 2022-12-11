cb64071433b2b3552c5bf971a3478a086c66e61e
Donnie Laughton <donnie.laughton@recursionpharma.com>
2022-12-11 05:04:14 -0500

    solved

--- 2022/day11/day11.py
+++ 2022/day11/day11.py
@@ -1,4 +1,6 @@
 import re
+import cProfile
+from collections import deque
 
 def parse_input(data):
     regex = r"""(?:Monkey (?P<monkey>\S+):\n.*Starting items: (?P<items>.+)\n.*Operation: new = old (?P<operation>.+)\n.*Test: divisible by (?P<test>.+)\n.*If true: throw to monkey (?P<case_true>.+)\n.*If false: throw to monkey (?P<case_false>.+))"""
@@ -7,9 +9,10 @@ def parse_input(data):
     for match in matches:
         monkey = dict()
         monkey['id'] = int(match.groups()[0])
-        monkey['items'] = [int(i) for i in match.groups()[1].replace(",","").split()]
+        monkey['items'] = deque([(int(i)) for i in match.groups()[1].replace(",","").split()])
         monkey['operation'] = match.groups()[2].split()
         monkey['test'] = [int(match.groups()[3]),int(match.groups()[4]),int(match.groups()[5])]
+        monkey['inspections'] = 0
         monkeys.append(monkey)
     return monkeys
 
@@ -32,8 +35,9 @@ def testing(worry, test):
 def business(monkeys):
     for monkey in monkeys:
         while monkey['items']:
-            item = monkey['items'].pop(0)
+            item = monkey['items'].popleft()
             item = inspecting(item, monkey['operation'])
+            monkey['inspections'] += 1
             item = int((item/3)//1)
             target_monkey = testing(item, monkey['test'])
             monkeys[target_monkey]['items'].append(item)
@@ -41,18 +45,36 @@ def business(monkeys):
 
 def s1(data):
     monkeys = parse_input(data)
-    monkeys = business(monkeys)
+    for _ in range(20):
+        monkeys = business(monkeys)
+    inspections = sorted([monkey['inspections'] for monkey in monkeys])
+    return inspections[-1] * inspections[-2]
+
+def terrible_business(monkeys):
+    worry_reducer = 1
     for monkey in monkeys:
-        print(f"Monkey {monkey['id']}: {monkey['items']}")
-    pass
+        worry_reducer = worry_reducer * monkey['test'][0]
+    for monkey in monkeys:
+        while monkey['items']:
+            item = monkey['items'].popleft()
+            item = inspecting(item, monkey['operation'])
+            monkey['inspections'] += 1
+            item = item % worry_reducer
+            target_monkey = testing(item, monkey['test'])
+            monkeys[target_monkey]['items'].append(item)
+    return monkeys
 
 def s2(data):
-    pass
+    monkeys = parse_input(data)
+    for _ in range(10000):
+        monkeys = terrible_business(monkeys)
+    inspections = sorted([monkey['inspections'] for monkey in monkeys])
+    return inspections[-1] * inspections[-2]
 
-with open("./2022/day11/test_day11_data.txt") as f:
+with open("./2022/day11/day11_data.txt") as f:
     data = f.read()
     print(f" s1: {s1(data)}")
-
-with open("./2022/day11/test_day11_data.txt") as f:
+    
+with open("./2022/day11/day11_data.txt") as f:
     data = f.read()
     print(f" s2: {s2(data)}")
