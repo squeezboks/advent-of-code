494d8773e3a0d22b0db974b2277b49403d8c8a26
Donnie Laughton <donnie.laughton@recursionpharma.com>
2023-12-02 09:39:42 -0500

    last commit for 2023

--- 2022/day13/day13.py
+++ 2022/day13/day13.py
@@ -1,13 +1,55 @@
 import multiprocessing
-from queue import PriorityQueue
 
 def parse_input(input):
     data = input.split('\n\n')
-    for row in data:
-        print(row)
+    packets = []
+    for pair in data:
+        left = pair.split()[0]
+        right = pair.split()[1]
+        packets.append((eval(left), eval(right)))
+    return packets
+
+def compare_lists(left, right):
+    #check if left and right are not empty
+    if left and right:
+        # there are items in both lists, compare the items
+        for index in range(min(len(left),len(right))):
+            if isinstance(left[index], list):
+                if isinstance(right[index], list):
+                    result = compare_lists(left[index], right[index])
+                else:
+                    result = compare_lists(left[index], list(right[index]))
+                
+                # check result is valid
+                if result != 0:
+                        return result
+            else:
+                if isinstance(right[index], list):
+                    result = compare_lists(list(left[index]), right[index])
+                else:
+                    lval = int(left[index])
+                    rval = int(right[index])
+                    if lval == rval:
+                        result = 0
+                    else:
+                        result = 1 if lval < rval else -1
+                
+                # check result is valid
+                if result != 0:
+                        return result
+        return 0
+    else:
+        # this covers the empty lists case
+        return 0
 
 def s1(data):
-    parse_input(data)
+    packets = parse_input(data)
+    result = []
+    for index, pair in enumerate(packets, start=1):
+        print(f"== Pair {index} ==")
+        result.append(compare_lists(*pair))
+
+    print(result)
     return 'not solved'
 
 def s2(data):
