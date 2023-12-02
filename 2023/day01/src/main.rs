6289132ed71339fecc0ca3186c762d7284fb4660
Donnie Laughton <donnie.laughton@recursionpharma.com>
2023-12-02 11:12:25 -0500

    day01: initial file reader

---
+++ 2023/day01/src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let file_path = &args[1];
+    
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With test:\n{contents}");
+}
